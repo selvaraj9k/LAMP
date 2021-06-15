c6a64a38cb54521b4c818e0e8a9c7d7d8a833c3f
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-06-15 13:32:11 +0530

    Mysql configuration

--- Lamp_Install/Mysql_Config/mysql.sql
+++ Lamp_Install/Mysql_Config/mysql.sql
@@ -1,4 +1,7 @@
 /* Mysql Configration after Installtion*/
+/*
+source mysql.sql
+*/
 SELECT user,authentication_string,plugin,host FROM mysql.user;
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '23-Apr-2001';
 FLUSH PRIVILEGES;
